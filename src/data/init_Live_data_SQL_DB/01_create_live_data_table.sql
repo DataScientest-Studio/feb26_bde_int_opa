0d4506f68a286171659552c4740473b9897479a0
Raed Ayadi <raedayadi3@gmail.com>
2026-04-21 12:49:49 +0200

    create an SQL DB to store raw and transformed streaming data

---
+++ src/data/init_Live_data_SQL_DB/01_create_live_data_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS klines_15m (
+            symbol TEXT,
+            open_time TIMESTAMPTZ,
+            open DOUBLE PRECISION,
+            high DOUBLE PRECISION,
+            low DOUBLE PRECISION,
+            close DOUBLE PRECISION,
+            volume DOUBLE PRECISION,
+            close_time TIMESTAMPTZ,
+            quote_asset_volume DOUBLE PRECISION,
+            trades INT,
+            taker_base DOUBLE PRECISION,
+            taker_quote DOUBLE PRECISION,
+            PRIMARY KEY (open_time)
+);
